1475fed622912d9a16d873a965851e4f0ef4b40b
bazylcossac <bazyl.cossac@gmail.com>
2025-04-03 09:45:20 +0200

    backend init, icon changed, packages installed

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const socket = require("socket.io");
+const { ExpressPeerServer } = require("peer");
+const PORT = 3000;
+
+const app = express();
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log("server running on port ", PORT);
+});
+
+const peerServer = ExpressPeerServer(server, { debug: true });
+peerServer.on("connection", (id) => {
+  console.log(`user conencted with ${id} id`);
+});
+
+app.use("/peerjs", peerServer);
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
