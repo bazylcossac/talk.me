7e41116ea8f6f2e8b819c595d1e7891e6fc2f67b
bazylcossac <bazyl.cossac@gmail.com>
2025-04-13 18:02:59 +0200

    chat form setup

--- frontend/src/components/main-call/chat-button/CallChat.tsx
+++ frontend/src/components/main-call/chat-button/CallChat.tsx
@@ -1,7 +1,26 @@
+import { Input } from "@/components/ui/input";
+import { FormEvent, useRef } from "react";
+import { IoMdSend } from "react-icons/io";
 function CallChat() {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const sendMessage = (e: FormEvent) => {
+    e.preventDefault();
+    formRef!.current!.reset();
+  };
+
   return (
-    <div className="bg-[#222222] md:w-[250px] w-full h-full rounded-md overflow-y-auto scrollbar-hide ">
-      CallChat
+    <div className="bg-[#222222] md:w-[250px] w-full h-full rounded-md overflow-y-auto scrollbar-hide flex">
+      <div className="mt-auto w-full p-2">
+        <form
+          onSubmit={sendMessage}
+          className="flex items-center justify-between gap-2"
+          ref={formRef}
+        >
+          <Input className="border-1 border-white/30" />
+          <IoMdSend className="text-xl hover:text-white/30 cursor-pointer" />
+        </form>
+      </div>
     </div>
   );
 }
