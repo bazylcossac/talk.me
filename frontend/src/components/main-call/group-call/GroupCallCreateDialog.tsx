d3a074368316e8b0233ace112bc5972eb0fa3aa9
bazylcossac <bazyl.cossac@gmail.com>
2025-04-22 17:06:18 +0200

    add show/hide password

--- frontend/src/components/main-call/group-call/GroupCallCreateDialog.tsx
+++ frontend/src/components/main-call/group-call/GroupCallCreateDialog.tsx
@@ -5,7 +5,8 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-
+import { FaRegEyeSlash } from "react-icons/fa";
+import { FaRegEye } from "react-icons/fa";
 import { Button } from "../../ui/button";
 
 import OutputSelect from "../../profile-dashboard/settings-dialog/OutputSelect";
@@ -29,6 +30,9 @@ function SettingsDialog({
   const [allCameras, setAllCamers] = useState<MediaDeviceInfo[]>([]);
   const [groupName, setGroupName] = useState("");
   const [groupPassword, setGroupPassword] = useState("");
+  const [passwordInputType, setPasswordInputType] = useState<
+    "text" | "password"
+  >("password");
 
   useEffect(() => {
     const getMediaDevices = async () => {
@@ -55,8 +59,6 @@ function SettingsDialog({
     });
   }, [allDevices]);
 
-  console.log(allOutputs);
-
   return (
     <dialog>
       <Dialog open={createDialogVisible} onOpenChange={setCreateDialogVisible}>
@@ -70,26 +72,43 @@ function SettingsDialog({
             </DialogHeader>
 
             <div className="flex flex-col gap-4">
-              <label htmlFor="group-name" className="text-xs">
-                Group name
-              </label>
-              <Input
-                type="text"
-                id="group-password"
-                placeholder="Group name"
-                value={groupName}
-                onChange={(e) => setGroupName(e.target.value)}
-              />
-              <label htmlFor="group-name" className="text-xs">
-                Group password
-              </label>
-              <Input
-                type="password"
-                id="group-password"
-                placeholder="Group password"
-                value={groupPassword}
-                onChange={(e) => setGroupPassword(e.target.value)}
-              />
+              <div>
+                <label htmlFor="group-name" className="text-xs">
+                  Group name
+                </label>
+                <Input
+                  type="text"
+                  id="group-password"
+                  placeholder="Group name"
+                  value={groupName}
+                  onChange={(e) => setGroupName(e.target.value)}
+                />
+              </div>
+              <div className="relative">
+                <label htmlFor="group-name" className="text-xs">
+                  Group password
+                </label>
+                <Input
+                  type={passwordInputType}
+                  id="group-password"
+                  placeholder="Group password"
+                  value={groupPassword}
+                  onChange={(e) => setGroupPassword(e.target.value)}
+                />
+                <button
+                  onClick={() =>
+                    setPasswordInputType((prev) =>
+                      prev === "password" ? "text" : "password"
+                    )
+                  }
+                >
+                  {passwordInputType === "password" ? (
+                    <FaRegEyeSlash className="absolute right-4 top-8.25 text-white/70 hover:text-white  " />
+                  ) : (
+                    <FaRegEye className="absolute right-4 top-8.25 text-white/70" />
+                  )}
+                </button>
+              </div>
             </div>
             <p className="font-bold">Your devices</p>
             <div className="flex flex-row justify-between items-center">
